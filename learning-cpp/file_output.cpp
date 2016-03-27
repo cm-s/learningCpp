0a818526cef9b05a08b49da0a4ac8b5436491fc9
cm-s <blufyr3530@gmail.com>
2016-03-26 22:50:09 -0700

    Added vars, input to file.

--- learning-cpp/file_output.cpp
+++ learning-cpp/file_output.cpp
@@ -1,11 +1,23 @@
 #include <fstream>
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main ()
 {
-   ofstream outfile;
-   outfile.open("file.dat", ios::out | ios::in);
+    string lastName, firstName;
+
+    //creating file in write mode(outfile mode) and input mode?
+    ofstream outfile;
+    outfile.open("file.dat", ios::out | ios::in);
+
+    cout << "Enter the first and last name:";
+    cin >> firstName;
+    cin >> lastName;
+
+    //storing vars value's in file.dat...Does this need to be seperate?
+    outfile << firstName << endl;
+    outfile << lastName << endl;
 
    return 0;
 }
