8b216ed65d1a411a74eb6f904065a029ea63e9a4
cm-s <blufyr3530@gmail.com>
2016-03-26 22:29:39 -0700

    Tested syntaxes, added semicolins.

--- learning-cpp/file_output.cpp
+++ learning-cpp/file_output.cpp
@@ -1,11 +1,11 @@
 #include <fstream>
 #include <iostream>
 using namespace std;
- 
+
 int main ()
 {
-   fstream file
-   file.open("file.dat", out | in | turnc)
+   ofstream outfile;
+   outfile.open("file.dat", out | in | turnc);
 
    return 0;
 }
