ae8c226b7cab5f02f3ab4ee25dcd8d08816a8d9a
cm-s <blufyr3530@gmail.com>
2016-03-30 14:40:09 -0700

    Fixed case checking.

--- learning-cpp/Exercism/transcription.cpp
+++ learning-cpp/Exercism/transcription.cpp
@@ -3,84 +3,70 @@
 #include <cstring>
 using namespace std;
 
-char convert_gene(char user_input);
+char convert_gene(char main_input);
 
 int main()
 {
     //discovering input gene
-    bool is_valid;
     string user_input;
-    char gene_creation[0] = {};
+    bool is_valid;
 
     cout << "Enter your gene string.";
-    cout << "\nThis string can only be comprised of capital letters A, C, U, G or T.";
+    cout << "\nThis string can only be letters A, C, U, G or T." << endl;
     cin >> user_input;
 
-    for (size_t it = 0; it < user_input.length(); it++) {
-        strcat(gene_creation, "0");
-    };
 
-    do {
-        for (int wd = 0; wd == user_input.length(); wd++) {
-            if (isalnum(user_input[wd]) == true) {
-                is_valid = false;
-            };
-
-            switch (user_input[wd]) {//breaking for good practice
-                case 'A':
-                    continue;
-                    break;
-                case 'c':
-                    continue;
-                    break;
-                case 'U':
-                    continue;
-                    break;
-                case 'G':
-                    continue;
-                    break;
-                case 'T':
-                    continue;
-                    break;
-                default:
-                    cout << "Err: This input is not valid.";
-                    is_valid = false;
-                    break;
-            };
+    for (int wd = 0; wd < user_input.length(); wd++) {
+        if (isalnum(user_input[wd]) == true) {
+            is_valid = false;
         };
-    } while(is_valid != true);
 
-    /*This tidbit shows output revealing that is_valid isn't set right
-    somewhere in the program and does not function as designed.*/
-    return is_valid;//delete
-
-return 0;
-} //blocks here and up run but do not return the desired values
-/*one occuring output of this code is "*** stack smashing detected ***".
-this could possibly be due to the gene_creation array size being preset to
-zero and "leaglly" having multiple values concatinated onto it.*/
-
-/*this code has never been tested in harmony with the code above*/
-char convert_gene(char user_input) {
-    char converted_gene = user_input;
-
-    for (int itm = 0; itm == strlen(user_input); itm++) {
-
-        switch (converted_gene) {
-            case 'G':
-                strcpy(converted_gene[itm], 'C');
+        switch (toupper(user_input[wd])) {
+            case 'A':
+                is_valid = false;
                 break;
             case 'C':
-                strcpy(converted_gene[itm], 'G');
+                is_valid = false;
+                break;
+            case 'U':
+                is_valid = false;
+                break;
+            case 'G':
+                is_valid = false;
                 break;
             case 'T':
-                strcpy(converted_gene[itm], 'A');
+                is_valid = false;
                 break;
-            case 'A':
-                strcpy(converted_gene[itm], 'U');
+            default:
+                cout << "Err: This input is not valid.";
+                is_valid = false;
                 break;
         };
     };
 
-    return converted_gene;
+return 0;
+} //blocks here and up run
+/*
+char convert_gene(char main_input) {
+    gene_size = sizeof()
+
+    for (size_t itm = 0; itm < strlen(main_input); itm++) {
+        switch (main_input[itm]) {
+            case 'g':
+                main_input[itm] = 'c';
+                break;
+            case 'c':
+                main_input[itm] = 'G';
+                break;
+            case 'T':
+                main_input[itm] = 'A';
+                break;
+            case 'A':
+                main_input[itm] = 'U';
+                break;
+        };
+    };
+
+    return main_input;
 }
+*/
