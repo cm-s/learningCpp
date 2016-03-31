c6a5666296b7a25782012660ad35c68b742c0c7b
cm-s <blufyr3530@gmail.com>
2016-03-30 17:21:49 -0700

    Perfected main(). Whole program runs.

--- learning-cpp/Exercism/transcription.cpp
+++ learning-cpp/Exercism/transcription.cpp
@@ -3,7 +3,7 @@
 #include <cstring>
 using namespace std;
 
-char convert_gene(char main_input);
+void convert_gene(string user_input, bool is_valid);
 
 int main()
 {
@@ -23,50 +23,52 @@ int main()
 
         switch (toupper(user_input[wd])) {
             case 'A':
-                is_valid = false;
+                is_valid = true;
                 break;
             case 'C':
-                is_valid = false;
+                is_valid = true;
                 break;
             case 'U':
-                is_valid = false;
+                is_valid = true;
                 break;
             case 'G':
-                is_valid = false;
+                is_valid = true;
                 break;
             case 'T':
-                is_valid = false;
+                is_valid = true;
                 break;
             default:
-                cout << "Err: This input is not valid.";
+                cout << "Err: This input is not valid." << endl;
                 is_valid = false;
                 break;
         };
     };
+    convert_gene(user_input, is_valid);
 
 return 0;
-} //blocks here and up run
-/*
-char convert_gene(char main_input) {
-    gene_size = sizeof()
+}
 
-    for (size_t itm = 0; itm < strlen(main_input); itm++) {
-        switch (main_input[itm]) {
-            case 'g':
-                main_input[itm] = 'c';
+void convert_gene(string user_input, bool is_valid) {
+    if (is_valid = true)
+    {
+    int gene_size = sizeof(user_input)/sizeof(user_input[0]);
+
+    for (int itm = 0; itm <= gene_size; itm++) {
+        switch (user_input[itm]) {
+            case 'G':
+                user_input[itm] = 'C';
                 break;
-            case 'c':
-                main_input[itm] = 'G';
+            case 'C':
+                user_input[itm] = 'G';
                 break;
             case 'T':
-                main_input[itm] = 'A';
+                user_input[itm] = 'A';
                 break;
             case 'A':
-                main_input[itm] = 'U';
+                user_input[itm] = 'U';
                 break;
+            };
         };
+        cout << user_input;
     };
-
-    return main_input;
-}
-*/
+};
