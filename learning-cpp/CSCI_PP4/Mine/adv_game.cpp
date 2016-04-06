cef15202c4635d63387814296cdbc2b30e627c04
cm-s <blufyr3530@gmail.com>
2016-04-05 20:19:35 -0700

    Not functional. Furthered function development.

--- learning-cpp/CSCI_PP4/Mine/adv_game.cpp
+++ learning-cpp/CSCI_PP4/Mine/adv_game.cpp
@@ -7,12 +7,15 @@ void erect_border(int grid_size, string grid);
 
 void grid_constructor(int grid_size, string grid);
 
+void character_dynamic(char& palyer, string& grid, char& move_direction);
+
 int main() {
     int grid_size;
     int x; int y;
     char move_direction;//fill at end
+    char palyer;
 
-    cout << "What size would you like the playing board to be";
+    cout << "Enter board size: ";
 
     cin >> grid_size;
     x = grid_size;
@@ -27,36 +30,37 @@ int main() {
         cout << endl;
     };
     cout << endl;
-    for (size_t ln =0; ln < grid_size; ln++) {
-        grid[ln][0] = '0';
-        grid[0][ln] = '0';
-        grid[ln][(grid_size -1)] = '0';
-        grid[(grid_size -1)][ln] = '0';
-    };
-    for (size_t row =0; row <= (y -1); row++) {
-        for (size_t col =0; col <= (x -1); col++) {
-            cout << grid[row][col] << ' ';
-        };
-        cout << endl;
-    };
+    erect_border(grid_size, grid);
+    grid_constructor(grid_size, grid, x, y);
+    character_dynamic(player, grid, move_direction);
 
     cout << "\nUse the WASD keys to move your character" << endl;
-    cin >> move_direction;
 
-    //erect_border(grid_size, grid);
 return 0;
 };
 
-void erect_border(int grid_size, string& grid) {
+void erect_border(int grid_size, string& gridState) {
+    for (size_t ln =0; ln < grid_size; ln++) {
+        gridState[ln][0] = '0';
+        gridState[0][ln] = '0';
+        gridState[ln][(grid_size -1)] = '0';
+        gridState[(grid_size -1)][ln] = '0';
+    };
+};//function complete
 
-};
-
-void grid_constructor(int grid_size, string grid, int x, int y) {
-    /*for (int row =0; row <= (y -1); row++) {
-        for (int col =0; col <= (x -1); col++) {
-            cout << grid[row][col] << ' ';
+void grid_constructor(int grid_size, string gridState, int x, int y) {
+    for (size_t row =0; row <= (y -1); row++) {
+        for (size_t col =0; col <= (x -1); col++) {
+            cout << gridState[row][col] << ' ';
         };
         cout << endl;
-    };*/
-    //this code will be needed
+    };
+};//function complete
+
+void character_dynamic(char& palyer, string& grid, char& move_direction) {
+    //place character in center initially
+    grid[(grid_size -1)/2][(grid_size -1)/2] = '&';
+    grid_constructor(grid_size, grid, x, y);
+    cin >> move_direction; cout << endl;
+
 };
