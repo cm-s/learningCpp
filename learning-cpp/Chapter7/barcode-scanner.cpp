f3e1eda142da34d9f345ecafb056ae6d7ce8f67b
cm-s <blufyr3530@gmail.com>
2016-04-06 23:19:15 -0700

    Debigging. Converted value types. Changed methods.

--- learning-cpp/Chapter7/barcode-scanner.cpp
+++ learning-cpp/Chapter7/barcode-scanner.cpp
@@ -6,9 +6,9 @@
  * Date created: 3/1/16
  */
 
-#include <thread>
 #include <cstring>
 #include <iostream>
+#include <string>
 
 using namespace std;
 
@@ -28,19 +28,19 @@ private:
      *
      * @param input barcode to be encoded.
      */
-    void binEncode(int input);
+    void binEncode(string input);
     /**
      * Decodes five digit code.
      *
      * @param input code to be decoded.
      */
-    void numDecode(int input);
+    void numDecode(string input);
 }
 
 int main()
 {
 //varaible holding value for the user's input.
-int input;
+string input;
 //requesting input from the user. Storing in input.
 cout << "Input a barcode or a five digit number.";
 cin >> input;
@@ -53,18 +53,20 @@ else
 return 0;
 }
 
-void binEncode(int input) {
+void binEncode(string input) {
+    //creating variable to represent the size of the input.
+    int size = sizeof(input)/sizeof(input[0]);
     //list representing seperated individual bits.
-    int plist[];
+    char plist[27];
     //at first, this is the list representing seperated groups of processed binary values.
     //at the end of the for loop, at the end of this scope, this list represents all processed values.
-    int bitlst[];
+    int bitlst[5];
     //variable representing output of final encoded barcode.
     int finOut;
 
     //loop seperating input binary list into an array. Storing seperated values in plist.
-    for (size_t it = 0; it < input.length(); it++)
-        plist.append(input[it]);
+    for (size_t it = 0; it < size; it++)
+        plist += input[it];
 
     //removing the digits "1" on either side of plist.
     plist.erase(0);
@@ -82,7 +84,7 @@ void binEncode(int input) {
     for (size_t item = 0; item < bitlst.length(); item++) {
 
         //looping through individual digits in the given group. Processing them according to place value.
-        for (size_t d = 0; d < item.length(); d++) {
+        for (size_t d = 0; d < item; d++) {
             switch(d) {
 				case 0:
 					item[d] *= 7;
@@ -98,62 +100,66 @@ void binEncode(int input) {
 				    break;
 				case 4:
 				    item[d] *= 0;
+                    break;
 			};
         };
 
         //looping through individual digits in the newly processed group.
         //sequentially adding each digit to the next one in the sequence.
         //storing newly added values in finOut.
-        for (size_t d = 0; d < item.length(); d++)
+        for (size_t d = 0; d < item; d++)
             finOut += item[d +1];
 
         //rewriting each group in bitlst with the single digit processed value sums.
-        item = finOut
+        item = finOut;
 
         //concenating all processed values in bitlst and storing in numcode aspect of zipCode.
-        zipCode::numcode = bitlst.join()
+        zipCode.numcode = bitlst.join();
     };
 
-}
+};
 
-void numDecode(int input) {
+void numDecode(string input) {
 	//placeholder for binary digit groups
-	int byteList[];
+	char byteList[25];
+    //creating variable to represent the size of the input.
+    int size = sizeof(input)/sizeof(input[0]);
 
     //looping through each character in input code. Adding to diglst.
-    for (size_t p = 0; p < input.length(); p++)
+    for (size_t p = 0; p < size; p++) {
 
 		//adding binary translations to list depending on what value the digit has.
 		//appending respective translation to byteList.
 		switch(p) {
 			case 9:
-				byteList.append("10100");
+				strcat(byteList, "10100");
 				break;
 			case 8:
-				byteList.append("10010");
+				strcat(byteList, "10010");
 				break;
 			case 7:
-				byteList.append("01110"); //10000 is an alternate value that is equal to seven
+				strcat(byteList, "01110"); //10000 is an alternate value that is equal to seven
 				break;
 			case 6:
-				byteList.append("01100");
+				strcat(byteList, "01100");
 				break;
 			case 5:
-				byteList.append("01010");
+				strcat(byteList, "01010");
 				break;
 			case 4:
-				byteList.append("01000");
+				strcat(byteList, "01000");
 				break;
 			case 3:
-				byteList.append("00110");
+				strcat(byteList, "00110");
 				break;
 			case 2:
-				byteList.append("00100");
+				strcat(byteList, "00100");
 				break;
 			case 1:
-				byteList.append("00010");
+				strcat(byteList, "00010");
 				break;
 		};
+    };
 	//not sure if doing this is legal
-	zipCode::binary = "1" << byteList.join() << "1";
+	zipCode.binary = "1" << byteList.join() << "1";
 };
