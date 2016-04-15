d2dc0e97e33b523a7c4cdb525cde88f2c65a6f48
cm-s <blufyr3530@gmail.com>
2016-04-15 11:29:02 -0700

    Completed player movement & wall collision detection.

--- learning-cpp/CSCI_PP4/Mine/alternative.cpp
+++ learning-cpp/CSCI_PP4/Mine/alternative.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <cstring>
 #include <time.h>
+#include <stdlib.h>
 using namespace std;
 
 class player
@@ -12,9 +13,11 @@ public:
     char skin = '&';
 };
 
+void gen_struct(char current_grid[20][20],  int grid_size);
+
 void display_grid(char current_grid[20][20], int grid_size);
 
-void character_dynamic(char current_grid[20][20], int grid_size, player character, char move_direction, bool gameOver);
+void character_dynamic(char current_grid[20][20], int grid_size, player &character, char move_direction, bool& gameOver);
 
 int main()
 {
@@ -66,6 +69,8 @@ char current_grid[20][20] = {
 char move_direction;
 bool gameOver = false;
 
+gen_struct(current_grid, grid_size);
+
 player character;
 character_dynamic(current_grid, grid_size, character, move_direction, gameOver);
 
@@ -81,38 +86,75 @@ void display_grid(char current_grid[20][20], int grid_size) {
     };
 };
 
-void character_dynamic(char current_grid[20][20], int grid_size, player character, char move_direction, bool gameOver) {
-    cout << "\nUse the WASD keys to move your charater." << endl;
+void character_dynamic(char current_grid[20][20], int grid_size, player &character, char move_direction, bool& gameOver) {
+    cout << "\nUse the WASD keys to move your character." << endl;
     do {
         current_grid[character.x][character.y] = character.skin;
         display_grid(current_grid, grid_size);
-        for (size_t md = 1; md <= 15; md++) {
-            //making space for new grid
-            for (size_t space = 0; space <= 15; space++) {
-                cout << endl;
-            }
-            //changing character's coords depending on the move_direction
-            switch (toupper(move_direction)) {
-                case 'W':
-                   character.y += 1;
-                case 'A':
-                   character.x -= 1;
-                case 'S':
-                   character.y -= 1;
-                case 'D':
-                   character.x += 1;
+            for (size_t md = 1; md <= 15; md++) {
+                int prev_x_coord = character.x;
+                int prev_y_coord = character.y;
+                //making space for new grid
+                for (size_t space = 0; space <= 15; space++) {
+                    cout << endl;
+                }
+                //changing character's coords depending on the move_direction
+                switch (toupper(move_direction)) {
+                    case 'W':
+                        character.x -= 1;
+                        break;
+                    case 'A':
+                        character.y -= 1;
+                        break;
+                    case 'S':
+                        character.x += 1;
+                        break;
+                    case 'D':
+                        character.y += 1;
+                        break;
+                    };
+                //checking if character is moving into a wall
+                if (current_grid[character.x][character.y] == '#') {
+                    cout << "\nYou cannot move here." << endl;
+                    character.x = prev_x_coord;//settings player to previous coords (before moved into a wall)
+                    character.y = prev_y_coord;//
+                    display_grid(current_grid, grid_size);
+                    cin >> move_direction;
+                } else {
+                    current_grid[character.x][character.y] = character.skin;
+                    //printing grid again
+                    display_grid(current_grid, grid_size);
+                    //replacing last cell that the character was in
+                    current_grid[character.x][character.y] = ' ';
+                    cin >> move_direction;
+                };
+            continue;
             };
-            //checking if character is moving into a wall
-            //if (current_grid[character.x][character.y] == current_grid[charater.x][]) {
-                //cout << "\nYou cannot move here." << endl;
-            //} else {
-                current_grid[character.x][character.y] = character.skin;
-            //};
-            //printing grid again
-            display_grid(current_grid, grid_size);
-            //replacing last cell that the charater was in
-            current_grid[character.x][character.y] = ' ';
-            cin >> move_direction;
-        };
+            if (character.x == 12 && character.y == 6) {
+                cout << "You've won the game!" << endl;
+                gameOver = true;
+            };
     } while(gameOver = false);
+};
+
+void gen_struct(char current_grid[20][20], int grid_size) {
+    //int random = rand() % 19 + 3;
+    int random = 10;
+    const char templateStruct[5][5] = {
+        {'#','#','#','#','#'},
+        {'#','*',' ',' ','#'},
+        {'#','#','#',' ','#'},
+        {'#','*',' ',' ','#'},
+        {'#','#','#',' ','#'},
+    };
+
+    for (size_t rcol = random; rcol < (random +5); rcol++) {
+        for (size_t rrow = random; rrow < (random +5); rrow++) {
+            for (size_t col = 0; col < 5; col++) {
+                for (size_t row = 0; row < 5; row++) {
+                    current_grid[rcol][rrow] = templateStruct[col][row];
+                };
+            };
+        };
+    };
 };
